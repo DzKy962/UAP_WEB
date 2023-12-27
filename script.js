9182e49309553714db7c7b184c6833c96950b495
DzKy962 <dzakyfadhilla962@gmail.com>
2023-12-27 15:57:17 +0700

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const harga = [{value:10.000},{value:20.000}, {value:30.000}]
+
+function calculate(){
+    const num1=parseFloat(document.getElementById("kode").value)
+    const num2=parseFloat(document.getElementById("harga").value)
+
+    if(num1="001"){
+        
+    }
+}
